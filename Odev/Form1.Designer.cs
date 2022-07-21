cc8564d9b40cb6639309c3433575db4a57a0856f
Ozan <Ozan@DESKTOP-7O1B8LJ>
2022-07-21 21:12:13 +0300

    Add project files.

---
+++ Odev/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Odev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtMeslek = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbSehir = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbIlce = new System.Windows.Forms.ComboBox();
+            this.btnKaldir = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblKayitSayisi = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(27, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AD SOYAD";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(27, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "MESLEK";
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(154, 22);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(121, 20);
+            this.txtAdSoyad.TabIndex = 2;
+            // 
+            // txtMeslek
+            // 
+            this.txtMeslek.Location = new System.Drawing.Point(154, 65);
+            this.txtMeslek.Name = "txtMeslek";
+            this.txtMeslek.Size = new System.Drawing.Size(121, 20);
+            this.txtMeslek.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(27, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ŞEHIR";           
+            // 
+            // cmbSehir
+            // 
+            this.cmbSehir.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSehir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbSehir.FormattingEnabled = true;
+            this.cmbSehir.Location = new System.Drawing.Point(154, 100);
+            this.cmbSehir.Name = "cmbSehir";
+            this.cmbSehir.Size = new System.Drawing.Size(121, 21);
+            this.cmbSehir.TabIndex = 5;
+            this.cmbSehir.SelectedIndexChanged += new System.EventHandler(this.cmbSehir_SelectedIndexChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.CheckBoxes = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 229);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(586, 176);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Ad Soyad";
+            this.columnHeader1.Width = 135;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Meslek";
+            this.columnHeader2.Width = 181;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Şehir";
+            this.columnHeader3.Width = 153;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "İlçe";
+            this.columnHeader4.Width = 138;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaydet.Location = new System.Drawing.Point(30, 195);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(121, 28);
+            this.btnKaydet.TabIndex = 7;
+            this.btnKaydet.Text = "KAYDET";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(27, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 21);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "İLÇE";
+            this.label4.Visible = false;
+            // 
+            // cmbIlce
+            // 
+            this.cmbIlce.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbIlce.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cmbIlce.FormattingEnabled = true;
+            this.cmbIlce.Location = new System.Drawing.Point(154, 138);
+            this.cmbIlce.Name = "cmbIlce";
+            this.cmbIlce.Size = new System.Drawing.Size(121, 21);
+            this.cmbIlce.TabIndex = 9;
+            this.cmbIlce.Visible = false;
+            // 
+            // btnKaldir
+            // 
+            this.btnKaldir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaldir.Location = new System.Drawing.Point(169, 195);
+            this.btnKaldir.Name = "btnKaldir";
+            this.btnKaldir.Size = new System.Drawing.Size(118, 28);
+            this.btnKaldir.TabIndex = 10;
+            this.btnKaldir.Text = "SİL";
+            this.btnKaldir.UseVisualStyleBackColor = true;
+            this.btnKaldir.Click += new System.EventHandler(this.btnKaldir_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(303, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 28);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "TÜMÜNÜ SİL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblKayitSayisi
+            // 
+            this.lblKayitSayisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblKayitSayisi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblKayitSayisi.Location = new System.Drawing.Point(427, 138);
+            this.lblKayitSayisi.Name = "lblKayitSayisi";
+            this.lblKayitSayisi.Size = new System.Drawing.Size(84, 25);
+            this.lblKayitSayisi.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(330, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Kayıt Sayısı:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 405);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblKayitSayisi);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnKaldir);
+            this.Controls.Add(this.cmbIlce);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.cmbSehir);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtMeslek);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.TextBox txtMeslek;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbSehir;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbIlce;
+        private System.Windows.Forms.Button btnKaldir;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblKayitSayisi;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Odev/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string nameSurname;
+        string job;
+        string city;
+        string district;
+        int numberofRecords;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {                   
+            cmbSehir.Items.Add("Istanbul");
+            cmbSehir.Items.Add("Ankara");
+            cmbSehir.Items.Add("Izmir");
+            RecordsOfNumbers();
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            nameSurname = txtAdSoyad.Text;
+            job = txtMeslek.Text;
+            city = cmbSehir.Text;
+            district = cmbIlce.Text;
+            string[] info = {nameSurname,job,city,district};
+            
+
+            if (nameSurname !="" && job!= "" && city !="" && district !="")
+            {
+                ListViewItem lvi = new ListViewItem(info);
+                listView1.Items.Add(lvi);
+            }
+            else
+            {
+                MessageBox.Show("Missing information available", "WARNING!!!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            RecordsOfNumbers();
+        }
+
+        private void cmbSehir_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbSehir.Text =="Istanbul")
+            {
+                cmbIlce.Visible = true;
+                cmbIlce.Items.Clear();
+                cmbIlce.Items.Add("Beyoğlu");
+                cmbIlce.Items.Add("Şişli");
+                cmbIlce.Items.Add("Kadıköy");
+                cmbIlce.Items.Add("Beşiktaş");
+                cmbIlce.Items.Add("Fatih");
+                cmbIlce.Items.Add("Kağıthane");
+                cmbIlce.Items.Add("Adalar");
+                cmbIlce.Items.Add("Bahçelievler");
+                cmbIlce.Items.Add("Esenler");
+            }
+            else if (cmbSehir.Text == "Ankara")
+            {
+                cmbIlce.Visible = true;
+                cmbIlce.Items.Clear();
+                cmbIlce.Items.Add("Çankaya");
+                cmbIlce.Items.Add("Etimesgut");
+                cmbIlce.Items.Add("Beypazarı");
+                cmbIlce.Items.Add("Sincan");
+                cmbIlce.Items.Add("Keçiören");
+                cmbIlce.Items.Add("Yenimahalle");
+                cmbIlce.Items.Add("Haymana");
+                cmbIlce.Items.Add("Akyurt");
+                cmbIlce.Items.Add("Altındağ");
+            }
+            else
+            {
+                cmbIlce.Visible = true;
+                cmbIlce.Items.Clear();
+                cmbIlce.Items.Add("Aliağa");
+                cmbIlce.Items.Add("Balçova");
+                cmbIlce.Items.Add("Karşıyaka");
+                cmbIlce.Items.Add("Bayraklı");
+                cmbIlce.Items.Add("Bergama");
+                cmbIlce.Items.Add("Bornova");
+                cmbIlce.Items.Add("Çeşme");
+                cmbIlce.Items.Add("Selçuk");
+                cmbIlce.Items.Add("Konak");
+            }
+        }
+
+        private void btnKaldir_Click(object sender, EventArgs e)
+        {
+            
+            int choosenNumber = listView1.SelectedItems.Count;
+            foreach (ListViewItem registrationInfo in listView1.SelectedItems)
+            {
+                registrationInfo.Remove();
+            }
+            MessageBox.Show(choosenNumber + " Records deleted");
+            int newRecordsNumber = numberofRecords - choosenNumber;
+            numberofRecords = newRecordsNumber;
+            lblKayitSayisi.Text = numberofRecords.ToString();
+        }
+        private void RecordsOfNumbers()
+        {
+            numberofRecords = listView1.Items.Count;
+            lblKayitSayisi.Text = Convert.ToString(numberofRecords);         
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listView1.Clear();
+            RecordsOfNumbers();
+        }     
+    }
+}
